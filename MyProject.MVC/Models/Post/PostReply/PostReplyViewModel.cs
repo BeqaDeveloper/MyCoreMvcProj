968c4ca6eee183a71b87120a9817c1af8a2fef8c
Beqadeveloper <cheqsea@gmail.com>
2020-05-12 01:49:50 +0400

    Added Post and PostReply ViewModels

---
+++ MyProject.MVC/Models/Post/PostReply/PostReplyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.MVC.Models.Post.PostReply
+{
+    public class PostReplyViewModel
+    {
+        [Display(Name = "Id")]
+        public long Id { get; set; }
+        [Display(Name = "Reply Content")]
+        public string Content { get; set; }
+        [Display(Name = "Replied Date")]
+        public DateTime Created { get; set; }
+
+        public class PostReplyListViewModel
+        {
+
+            public PostReplyListViewModel()
+            {
+                PostReplyList = new List<PostReplyViewModel>();
+            }
+
+            public List<PostReplyViewModel> PostReplyList { get; set; }
+        }
+    }
+}
---
+++ MyProject.MVC/Models/Post/PostViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.MVC.Models.Post
+{
+    public class PostViewModel
+    {
+        [Display(Name = "Id")]
+        public long Id { get; set; }
+
+        [Display(Name = "Post Title")]
+        public string  Title { get; set; }
+
+        [Display(Name = "Author Name")]
+        public string AuthorName { get; set; }
+
+        [Display(Name = "AuthorRating")]
+        public long AuthorRating { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [Display(Name = "Data Posted")]
+        public string DatePosted { get; set; }
+
+        public List<SelectListItem> Forum { get; set; }
+        public List<SelectListItem> PostReply { get; set; }
+
+
+        public class PostListViewModel
+        {
+
+            public PostListViewModel()
+            {
+                PostList = new List<PostViewModel>();
+            }
+
+            public List<PostViewModel> PostList { get; set; }
+        }
+    }
+}
