5c69a14ae7d099fda843ffe4f4803c236ea17077
Beqadeveloper <cheqsea@gmail.com>
2020-05-12 00:15:07 +0400

    Added MappingPRofile

---
+++ MyProject.MVC/Controllers/ForumController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyProject.Domain.Interfaces;
+
+namespace MyProject.MVC.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+        private readonly IForumService forumService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyProject.MVC/Mapping/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.MVC.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<Forum, ForumViewModel>();
+            //CreateMap<Forum, ForumViewModel>().ReverseMap();
+        }
+    }
+}
